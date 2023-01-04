942b4ba688c8aea4744cf10a2f688e854d2a9f44
adambushman <adambushman@mail.weber.edu>
2023-01-04 10:15:51 -0700

    Reorganized scripts and workflow

--- gha_scripts/pbp_data_update.R
+++ gha_scripts/pbp_data_update.R
@@ -1,6 +1,7 @@
 require('DBI')
 require('duckdb')
 require('tibble')
+require('dplyr')
 require('hoopR')
 
 ###
@@ -16,17 +17,36 @@ con = DBI::dbConnect(
   read_only = FALSE
 )
 
-# Get PBP Data
+# Get current ids
+
+res = DBI::dbGetQuery(
+  con, 
+  'SELECT id FROM pbp'
+)
+
+# Get PBP data
 
 pbp_data <-
   hoopR::load_nba_pbp() |> 
   tibble::as_tibble()
 
+# New Ids to add
+
+new_ids <- setdiff(pbp_data$id, res |> unlist(use.names = FALSE) |> sample(200000))
+
+# Filter for new records
+
+pbp_data.new <-
+  pbp_data |>
+  dplyr::filter(id %in% new_ids)
+
+# Add new records
+
 DBI::dbWriteTable(
   con, 
   "pbp", 
-  as_tibble(pbp_data), 
-  overwrite = TRUE
+  as_tibble(pbp_data.new), 
+  append = TRUE
 )
 
 # Close Connection
